42cf0d182ad147f0da1ddddd6ffa411143492b79
huanniangstudio <891863891@qq.com>
2021-05-25 11:59:43 +0800

    fix

--- src/index.browser.ts
+++ src/index.browser.ts
@@ -1,4 +1,4 @@
-require("./common/fix_in_browser_hard");
+require("./common/fix_in_browser");
 export { SensibleNFT } from "./bcp01";
 export { SensibleFT } from "./bcp02";
 export { API_NET } from "./sensible-api";
