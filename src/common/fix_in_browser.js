ba400e1a8a9d653c0033442584e69200ac4e7e11
huanniangstudio <891863891@qq.com>
2021-06-19 15:20:12 +0800

    fix bug

--- src/common/fix_in_browser.js
+++ src/common/fix_in_browser.js
@@ -1,21 +1,17 @@
-function fixBuffer(__Buffer) {
-  if (!__Buffer.prototype.readBigUInt64LE) {
-    __Buffer.prototype.readBigUInt64LE = function (offset) {
-      let b1 = BigInt(this.readUInt32LE(offset + 0));
-      let b2 = BigInt(this.readUInt32LE(offset + 4));
-      return b1 + (b2 << BigInt(32));
-    };
-    __Buffer.prototype.writeBigUInt64LE = function (num) {
-      let b2 = num >> BigInt(32);
-      let b1 = num - (b2 << BigInt(32));
-      this.writeUInt32LE(parseInt(b1), 0);
-      this.writeUInt32LE(parseInt(b2), 4);
-    };
-  }
+if (!Buffer.prototype.readBigUInt64LE) {
+  Buffer.prototype.readBigUInt64LE = function (offset) {
+    let b1 = BigInt(this.readUInt32LE(offset + 0));
+    let b2 = BigInt(this.readUInt32LE(offset + 4));
+    return b1 + (b2 << BigInt(32));
+  };
+  Buffer.prototype.writeBigUInt64LE = function (num) {
+    let b2 = num >> BigInt(32);
+    let b1 = num - (b2 << BigInt(32));
+    this.writeUInt32LE(parseInt(b1), 0);
+    this.writeUInt32LE(parseInt(b2), 4);
+  };
 }
-fixBuffer(Buffer);
 if (typeof window !== "undefined" && typeof window.Buffer == "undefined") {
   const Buffer = require("buffer/index").Buffer;
   window.Buffer = Buffer;
-  fixBuffer(Buffer);
 }
